fb941a201e574520921e7aae86a7dd933fd89b15
LW <lw222ii@student.lnu.se>
2017-08-22 10:25:19 +0200

    fixed key gen helper

--- model/helpers/generateVAPIDandGCMkeys.js
+++ model/helpers/generateVAPIDandGCMkeys.js
@@ -1,9 +1,7 @@
 'use strict';
 const webpush = require('web-push');
 
-module.exports = () => {
-    let vapidKeys = webpush.generateVAPIDKeys();
+let vapidKeys = webpush.generateVAPIDKeys();
 
-    console.log('Public key: ', vapidKeys.publicKey);
-    console.log('Private key: ', vapidKeys.privateKey);
-};
+console.log('Public key: ', vapidKeys.publicKey);
+console.log('Private key: ', vapidKeys.privateKey);
