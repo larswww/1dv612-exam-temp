cf21a5e95c0a60b69b8f02ced41ddd6c724c0a73
LW <lw222ii@student.lnu.se>
2018-03-16 18:38:36 +0800

    new version for production

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2,10 +2,10 @@
 'use strict'
 
 //live
-// const connectionUrl = 'https://github.larsw.net/api/'
+const connectionUrl = 'https://github.larsw.net/api/'
 
 // local
-const connectionUrl = 'http://localhost:3000/api/'
+//const connectionUrl = 'http://localhost:3000/api/'
 
 var Sandbox = require('./facade')
 
@@ -710,10 +710,10 @@ var CORE = require('../core');
 
 CORE.create_module('webPushButton', function (sb) {
     //live
-    // const applicationServerPublicKey = 'BFKuHah3AIxUe0oXiWLeXJ8Yv79wmXRgHgjG2xKjymIuueQICb5E5OIUvAW033bvmfBaZi856_BhByhayfX1yFs';
+     const applicationServerPublicKey = 'BFKuHah3AIxUe0oXiWLeXJ8Yv79wmXRgHgjG2xKjymIuueQICb5E5OIUvAW033bvmfBaZi856_BhByhayfX1yFs';
 
     //local
-    const applicationServerPublicKey = 'BIslP8UZWMbRU3RjFFaVfM5-c2jqXw1eno9TVwjt69cJPHwbbtpNYaa99E6CHJ7o4ZPPZhvR5e6fOVa5KyLwg1I';
+    //const applicationServerPublicKey = 'BIslP8UZWMbRU3RjFFaVfM5-c2jqXw1eno9TVwjt69cJPHwbbtpNYaa99E6CHJ7o4ZPPZhvR5e6fOVa5KyLwg1I';
 
     const pushButton = document.querySelector('#pushNoticeButton');
 
